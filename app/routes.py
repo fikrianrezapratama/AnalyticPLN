bc510afdd321dc95591d34681c15dc0339c58972
fikrianrezapratama <84733739+fikrianrezapratama@users.noreply.github.com>
2024-06-12 14:17:22 +0700

    fixed api data_pln by ID

--- app/routes.py
+++ app/routes.py
@@ -64,11 +64,17 @@ def process_data_route_by_id(data_id):
 
     token = login_response['token']
     data = fetch_data_by_id(token, data_id)
+
     if data is None:
         return jsonify({"error": "Failed to fetch data"}), 500
 
-    process_data_id = process_data(data)
-    response_json = json.dumps(process_data_id, indent=2)
+    processed_data = process_data(data)
+
+    if str(data_id) not in processed_data:
+        return jsonify({"error": f"No processed data found for ID {data_id}"}), 404
+
+    response_data = {str(data_id): processed_data[str(data_id)]}
+    response_json = json.dumps(response_data, indent=2)
     return Response(response_json, mimetype='application/json')
     # return jsonify(data)
     
@@ -76,6 +82,7 @@ def process_data_route_by_id(data_id):
 def data_pln():
     login_response = login()
     if 'token' not in login_response:
+        logging.error("Failed to authenticate: No token in login response")
         return jsonify({"error": "Failed to authenticate"}), 401
 
     token = login_response['token']
@@ -85,7 +92,7 @@ def data_pln():
         logging.error("Fetched data is empty or does not contain 'data' key.")
         return jsonify({"error": "Failed to fetch data"}), 500
 
-    # Log the fetched data to inspect its structure
+    # cek Log fetched data
     # logging.info(f"Fetched data: {data['data']}")
 
     processed_data = process_data(data)
@@ -94,54 +101,56 @@ def data_pln():
         logging.error("Processed data is empty.")
         return jsonify({"error": "Failed to process data"}), 500
 
-    # Log the processed data to inspect its structure
+    # cek Log processed data
     # logging.info(f"Processed data: {processed_data}")
 
     combined_data = []
     for item in data['data']:
-        logging.info(f"Processing item: {item}")  # Log each item to inspect it
+        # logging.info(f"Processing item: {item}")  # Log each item to inspect it
         if isinstance(item, dict):
             item_id = item.get('id')
-            if item_id is not None and item_id in processed_data:
-                fetched_item = item
-                processed_item = processed_data[item_id]
-                combined_item = {
-                    "id": item_id,
-                    "user_id": fetched_item["user_id"],
-                    "latitude": fetched_item["latitude"],
-                    "longitude": fetched_item["longitude"],
-                    "address": fetched_item["address"],
-                    "photo": fetched_item["photo"],
-                    "no_meteran": fetched_item["no_meteran"],
-                    "capacity": fetched_item["capacity"],
-                    "energy_kredit": fetched_item["energy_kredit"],
-                    "tegangan": fetched_item["tegangan"],
-                    "arus": fetched_item["arus"],
-                    "power_limit": fetched_item["power_limit"],
-                    "power_factor": fetched_item["power_factor"],
-                    "phase_current_rms": fetched_item["phase_current_rms"],
-                    "neutral_current_rms": fetched_item["neutral_current_rms"],
-                    "tampering_status": fetched_item["tampering_status"],
-                    "terminal_covers_open": fetched_item["terminal_covers_open"],
-                    "meter_covers_open": fetched_item["meter_covers_open"],
-                    "power_outages": fetched_item["power_outages"],
-                    "kwh_credit_tokens_received": fetched_item["kwh_credit_tokens_received"],
-                    "technical_tokens_received": fetched_item["technical_tokens_received"],
-                    "tariff_index": fetched_item["tariff_index"],
-                    "read_code": fetched_item["read_code"],
-                    "lcd_condition": fetched_item["lcd_condition"],
-                    "terminal_quantity": fetched_item["terminal_quantity"],
-                    "seal_status": fetched_item["seal_status"],
-                    "meter_location": fetched_item["meter_location"],
-                    "user": fetched_item["user"],
-                    "created_at": fetched_item["created_at"],
-                    "status": processed_item["Hasil Analisis"]["status"],
-                    "message": processed_item["Hasil Analisis"]["message"]
-                }
-                logging.info(f"Combined item: {combined_item}")
-                combined_data.append(combined_item)
+            if item_id is not None:
+                processed_item = processed_data.get(str(item_id))  # Ensure the key is a string
+                if processed_item:
+                    fetched_item = item
+                    combined_item = {
+                        "id": item_id,
+                        "user_id": fetched_item["user_id"],
+                        "latitude": fetched_item["latitude"],
+                        "longitude": fetched_item["longitude"],
+                        "address": fetched_item["address"],
+                        "photo": fetched_item["photo"],
+                        "no_meteran": fetched_item["no_meteran"],
+                        "capacity": fetched_item["capacity"],
+                        "energy_kredit": fetched_item["energy_kredit"],
+                        "tegangan": fetched_item["tegangan"],
+                        "arus": fetched_item["arus"],
+                        "power_limit": fetched_item["power_limit"],
+                        "power_factor": fetched_item["power_factor"],
+                        "phase_current_rms": fetched_item["phase_current_rms"],
+                        "neutral_current_rms": fetched_item["neutral_current_rms"],
+                        "tampering_status": fetched_item["tampering_status"],
+                        "terminal_covers_open": fetched_item["terminal_covers_open"],
+                        "meter_covers_open": fetched_item["meter_covers_open"],
+                        "power_outages": fetched_item["power_outages"],
+                        "kwh_credit_tokens_received": fetched_item["kwh_credit_tokens_received"],
+                        "technical_tokens_received": fetched_item["technical_tokens_received"],
+                        "tariff_index": fetched_item["tariff_index"],
+                        "read_code": fetched_item["read_code"],
+                        "lcd_condition": fetched_item["lcd_condition"],
+                        "terminal_quantity": fetched_item["terminal_quantity"],
+                        "seal_status": fetched_item["seal_status"],
+                        "meter_location": fetched_item["meter_location"],
+                        "user": fetched_item["user"],
+                        "created_at": fetched_item["created_at"],
+                        "status": processed_item["Hasil Analisis"]["status"],
+                        "message": processed_item["Hasil Analisis"]["message"]
+                    }
+                    combined_data.append(combined_item)
+                else:
+                    logging.error(f"Processed data for ID {item_id} not found.")
             else:
-                logging.error(f"Processed data for ID {item_id} not found.")
+                logging.error(f"Item ID is None: {item}")
         else:
             logging.error(f"Item is not a dictionary: {item}")
 
@@ -153,6 +162,7 @@ def data_pln():
     response_json = json.dumps(response_data, indent=2)
     return Response(response_json, mimetype='application/json')
 
+
 @main.route('/data_pln/<int:data_id>', methods=['GET'])
 def data_pln_by_id(data_id):
     login_response = login()
@@ -160,51 +170,64 @@ def data_pln_by_id(data_id):
         return jsonify({"error": "Failed to authenticate"}), 401
 
     token = login_response['token']
-    data = fetch_data_by_id(token, data_id)
+    data_response = fetch_data_by_id(token, data_id)
 
-    if data is None:
+    if data_response is None:
         return jsonify({"error": "Failed to fetch data"}), 500
 
-    processed_data = process_data(data)
+    fetched_item = data_response.get('data', {})
+    if not fetched_item:
+        return jsonify({"error": "No data found"}), 404
+
+    # cek Log fetched_item
+    # logging.info(f"Fetched item: {fetched_item}")
+
+    # Wrap the fetched_item in a dictionary with a "data" key
+    processed_data = process_data({"data": fetched_item})
+
+    # cek Log processed_data
+    # logging.info(f"Processed data: {processed_data}")
 
     if str(data_id) not in processed_data:
         return jsonify({"error": f"No processed data found for ID {data_id}"}), 404
 
-    fetched_item = data
     processed_item = processed_data[str(data_id)]
 
+    # cek Log the processed_item
+    # logging.info(f"Processed item: {processed_item}")
+
     combined_item = {
-        "id": data_id,
-        "user_id": fetched_item["user_id"],
-        "latitude": fetched_item["latitude"],
-        "longitude": fetched_item["longitude"],
-        "address": fetched_item["address"],
-        "photo": fetched_item["photo"],
-        "no_meteran": fetched_item["no_meteran"],
-        "capacity": fetched_item["capacity"],
-        "energy_kredit": fetched_item["energy_kredit"],
-        "tegangan": fetched_item["tegangan"],
-        "arus": fetched_item["arus"],
-        "power_limit": fetched_item["power_limit"],
-        "power_factor": fetched_item["power_factor"],
-        "phase_current_rms": fetched_item["phase_current_rms"],
-        "neutral_current_rms": fetched_item["neutral_current_rms"],
-        "tampering_status": fetched_item["tampering_status"],
-        "terminal_covers_open": fetched_item["terminal_covers_open"],
-        "meter_covers_open": fetched_item["meter_covers_open"],
-        "power_outages": fetched_item["power_outages"],
-        "kwh_credit_tokens_received": fetched_item["kwh_credit_tokens_received"],
-        "technical_tokens_received": fetched_item["technical_tokens_received"],
-        "tariff_index": fetched_item["tariff_index"],
-        "read_code": fetched_item["read_code"],
-        "lcd_condition": fetched_item["lcd_condition"],
-        "terminal_quantity": fetched_item["terminal_quantity"],
-        "seal_status": fetched_item["seal_status"],
-        "meter_location": fetched_item["meter_location"],
-        "user": fetched_item["user"],
-        "created_at": fetched_item["created_at"],
-        "status": processed_item["Hasil Analisis"]["status"],
-        "message": processed_item["Hasil Analisis"]["message"]
+        "id": fetched_item.get("id", "N/A"),
+        "user_id": fetched_item.get("user_id", "N/A"),
+        "latitude": fetched_item.get("latitude", "N/A"),
+        "longitude": fetched_item.get("longitude", "N/A"),
+        "address": fetched_item.get("address", "N/A"),
+        "photo": fetched_item.get("photo", "N/A"),
+        "no_meteran": fetched_item.get("no_meteran", "N/A"),
+        "capacity": fetched_item.get("capacity", "N/A"),
+        "energy_kredit": fetched_item.get("energy_kredit", "N/A"),
+        "tegangan": fetched_item.get("tegangan", "N/A"),
+        "arus": fetched_item.get("arus", "N/A"),
+        "power_limit": fetched_item.get("power_limit", "N/A"),
+        "power_factor": fetched_item.get("power_factor", "N/A"),
+        "phase_current_rms": fetched_item.get("phase_current_rms", "N/A"),
+        "neutral_current_rms": fetched_item.get("neutral_current_rms", "N/A"),
+        "tampering_status": fetched_item.get("tampering_status", "N/A"),
+        "terminal_covers_open": fetched_item.get("terminal_covers_open", "N/A"),
+        "meter_covers_open": fetched_item.get("meter_covers_open", "N/A"),
+        "power_outages": fetched_item.get("power_outages", "N/A"),
+        "kwh_credit_tokens_received": fetched_item.get("kwh_credit_tokens_received", "N/A"),
+        "technical_tokens_received": fetched_item.get("technical_tokens_received", "N/A"),
+        "tariff_index": fetched_item.get("tariff_index", "N/A"),
+        "read_code": fetched_item.get("read_code", "N/A"),
+        "lcd_condition": fetched_item.get("lcd_condition", "N/A"),
+        "terminal_quantity": fetched_item.get("terminal_quantity", "N/A"),
+        "seal_status": fetched_item.get("seal_status", "N/A"),
+        "meter_location": fetched_item.get("meter_location", "N/A"),
+        "user": fetched_item.get("user", "N/A"),
+        "created_at": fetched_item.get("created_at", "N/A"),
+        "status": processed_item.get("Hasil Analisis", {}).get("status", "N/A"),
+        "message": processed_item.get("Hasil Analisis", {}).get("message", "N/A")
     }
 
     response_data = {"data": combined_item}
--- utils/data_processing.py
+++ utils/data_processing.py
@@ -1,62 +1,49 @@
-
-# utils/data_processing.py
 from utils.processing.filter_data import filter_data
 from utils.processing.compare_data import compare_data
 from utils.processing.average_data import calculate_average_voltage
-from utils.processing.processing_so import kriteria1, kriteria2,kriteria3, kriteria4, kriteria5, kriteria6, kriteria7, kriteria8, kriteria9, kriteria10, kriteria11, kriteria12, analisis_akhir
+from utils.processing.processing_so import (
+    kriteria1, kriteria2, kriteria3, kriteria4, 
+    kriteria5, kriteria6, kriteria7, kriteria8, 
+    kriteria9, kriteria10, kriteria11, kriteria12, 
+    analisis_akhir
+)
 
-# def process_data(data):
-#     """
-#     Process the data from the API using various methods.
-
-#     Args:
-#         data (dict): The raw data from the API.
-
-#     Returns:
-#         dict: Processed data including summaries and filters.
-#     """
-#     filtered_data = filter_data(data, condition=1)
-#     compared_data = compare_data(data, threshold=220.0)
-#     average_voltage = calculate_average_voltage(data)
-    
-#     return {
-#         'filtered_data': filtered_data,
-#         'compared_data': compared_data,
-#         'average_voltage': average_voltage
-#     }
-
-def process_data(data):
+def process_data(data, data_id=None):
     hasil = {}
+    
     if isinstance(data["data"], dict):
-        hasil["Kriteria 1"] = kriteria1(data["data"])
-        hasil["Kriteria 2"] = kriteria2(data["data"])
-        hasil["Kriteria 3"] = kriteria3(data["data"])
-        hasil["Kriteria 4"] = kriteria4 (data["data"])
-        # hasil["Kriteria 5"] = kriteria5 (data["data"])
-        # hasil["Kriteria 6"] = kriteria6 (data["data"])
-        hasil["Kriteria 7"] = kriteria7 (data["data"])
-        hasil["Kriteria 8"] = kriteria8 (data["data"])
-        hasil["Kriteria 9"] = kriteria9 (data["data"])
-        hasil["Kriteria 10"] = kriteria10 (data["data"])
-        # hasil["Kriteria 11"] = kriteria11 (data["data"])
-        # hasil["Kriteria 12"] = kriteria12 (data["data"])
-        hasil["Hasil Analisis"] = analisis_akhir (data["data"])
-        
+        if data_id is None or data_id == data["data"]["id"]:
+            hasil[str(data["data"]["id"])] = {
+                "Kriteria 1": kriteria1(data["data"]),
+                "Kriteria 2": kriteria2(data["data"]),
+                "Kriteria 3": kriteria3(data["data"]),
+                "Kriteria 4": kriteria4(data["data"]),
+                # "Kriteria 5": kriteria5(data["data"]),
+                # "Kriteria 6": kriteria6(data["data"]),
+                "Kriteria 7": kriteria7(data["data"]),
+                "Kriteria 8": kriteria8(data["data"]),
+                "Kriteria 9": kriteria9(data["data"]),
+                "Kriteria 10": kriteria10(data["data"]),
+                # "Kriteria 11": kriteria11(data["data"]),
+                # "Kriteria 12": kriteria12(data["data"]),
+                "Hasil Analisis": analisis_akhir(data["data"])
+            }
     else:
         for entry in data["data"]:
-            hasil[entry["id"]] = {
-                "Kriteria 1": kriteria1(entry), #sisa KWH tidak wajar (diatas>1000kwH)
-                "Kriteria 2": kriteria2(entry), #Tegangan = 0, Arus>0,1
-                "Kriteria 3": kriteria3(entry), #Arus = 0, Tegangan>180V, Faktor Daya = 0,1 - 0.9
-                "Kriteria 4": kriteria4(entry), #LCD Rusak, Error, Blank
-                # "Kriteria 5": kriteria5(entry), #Keypad Rusak
-                # "Kriteria 6": kriteria6(entry), #Kondisi rumah berpenghuni tetapi arus = 0
-                "Kriteria 7": kriteria7(entry), #Frekuensi input CT yang tinggi
-                "Kriteria 8": kriteria8(entry), #Tutup terminal dibuka
-                "Kriteria 9": kriteria9(entry), #status Tampering (1, 2, 4, 5)
-                "Kriteria 10": kriteria10(entry), #Arus netral > arus fasa
-                # "Kriteria 11": kriteria11(entry), #Arus > 0, Terakhir beli token lebih dari 3 bulan, kondisi LCD "Periksa"
-                # "Kriteria 12": kriteria12(entry), #Energi kumulatif tetap, jumlah input token tinggi
-                "Hasil Analisis": analisis_akhir(entry)
-            } 
+            if data_id is None or data_id == entry["id"]:
+                hasil[str(entry["id"])] = {
+                    "Kriteria 1": kriteria1(entry),
+                    "Kriteria 2": kriteria2(entry),
+                    "Kriteria 3": kriteria3(entry),
+                    "Kriteria 4": kriteria4(entry),
+                    # "Kriteria 5": kriteria5(entry),
+                    # "Kriteria 6": kriteria6(entry),
+                    "Kriteria 7": kriteria7(entry),
+                    "Kriteria 8": kriteria8(entry),
+                    "Kriteria 9": kriteria9(entry),
+                    "Kriteria 10": kriteria10(entry),
+                    # "Kriteria 11": kriteria11(entry),
+                    # "Kriteria 12": kriteria12(entry),
+                    "Hasil Analisis": analisis_akhir(entry)
+                }
     return hasil
